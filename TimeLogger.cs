964ed2191a6e217aefee8f1560f991b03dee4e23
gehao <sakura.gehao@gmail.com>
2021-08-09 13:14:06 +0800

    add test for download

---
+++ TimeLogger.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace DataTransfer
+{
+    public static class TimeLogger
+    {
+        private static Stopwatch stopWatch = new Stopwatch();
+        private static ILogger logger = null;
+
+        public static void setLogger(ILogger log)
+        {
+            logger = log;
+        }
+
+        public static bool startTimeLogger()
+        {
+            if (logger == null)
+            {
+                return false;
+            }
+            stopWatch.Start();
+            return true;
+        }
+
+        public static bool stopTimeLogger(string msg = null)
+        {
+            if (logger == null)
+            {
+                return false;
+            }
+            stopWatch.Stop();
+            logger.LogInformation(msg + ": spends " + stopWatch.ElapsedMilliseconds + " ms.");
+            return true;
+        }
+    }
+}
